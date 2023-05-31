edeec0d691aab52140ac84181864b27238d2d83a
juliedosher <juliegdosher@lewisu.edu>
2023-05-30 22:50:03 -0500

    fixed light toggle image

--- static/script/main.js
+++ static/script/main.js
@@ -4,13 +4,14 @@ function toggleLightMode() {
     if (isDark) {
         document.getElementById('lightingMode').setAttribute('href', './style/light.css');
         document.getElementById("nav").className = "navbar navbar-expand-sm bg-light navbar-light fixed-top";
-        document.getElementById("modeImage").src = './images/icons/darkMode.png';
+        document.getElementById("modeImage").src = './images/icons/moon.png';
         document.getElementById("githubImage").src = './images/icons/github-lightMode.png';
         isDark = false;
    
     } else {
         document.getElementById('lightingMode').setAttribute('href', './style/dark.css');
         document.getElementById("nav").className = "navbar navbar-expand-sm bg-dark navbar-dark fixed-top";
+        document.getElementById("modeImage").src = './images/icons/sun.png';
         document.getElementById("githubImage").src = './images/icons/github-darkMode.png';
         isDark = true;
     }
