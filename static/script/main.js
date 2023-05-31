6b8869e616e5ffa2d47fea0b52aa276fc9324f8a
juliedosher <juliegdosher@lewisu.edu>
2023-05-30 22:46:08 -0500

    completed Contact Me section

--- static/script/main.js
+++ static/script/main.js
@@ -5,12 +5,13 @@ function toggleLightMode() {
         document.getElementById('lightingMode').setAttribute('href', './style/light.css');
         document.getElementById("nav").className = "navbar navbar-expand-sm bg-light navbar-light fixed-top";
         document.getElementById("modeImage").src = './images/icons/darkMode.png';
+        document.getElementById("githubImage").src = './images/icons/github-lightMode.png';
         isDark = false;
    
     } else {
         document.getElementById('lightingMode').setAttribute('href', './style/dark.css');
         document.getElementById("nav").className = "navbar navbar-expand-sm bg-dark navbar-dark fixed-top";
-        document.getElementById("modeImage").src = './images/icons/lightMode.png';
+        document.getElementById("githubImage").src = './images/icons/github-darkMode.png';
         isDark = true;
     }
 }
